1f72ea024c7db2b7ea91c67fed07db48acab9079
KarlEbbe <karlebbej@gmail.com>
2016-04-08 16:48:51 +0200

    some changes

--- src/server/Controller.java
+++ src/server/Controller.java
@@ -163,7 +163,7 @@ public class Controller {
 	private void newActivity(ClientHandler clientHandler, JSONObject jsonObject){
 		String owner = (String) jsonObject.get(Constants.NAME);
 		String location = (String) jsonObject.get(Constants.PLACE);
-		String subcategory = (String) jsonObject.get(Constants.SUBCATEGORY); 
+		int subcategory = (int) jsonObject.get(Constants.SUBCATEGORY); 
 		int maxnbr = (int) jsonObject.get(Constants.MAX_NBROF_PARTICIPANTS);
 		int nbrOfParticipants = (int) jsonObject.get(Constants.NBR_OF_PARTICIPANTS);
 		int minnbr = (int) jsonObject.get(Constants.TIME);
@@ -172,7 +172,7 @@ public class Controller {
 		String message = (String) jsonObject.get(Constants.MESSAGE);
 		String headline = (String) jsonObject.get(Constants.HEADLINE); 
 		
-		if(databaseManager.addNewActivity(owner, location, subcategory, maxnbr, nbrOfParticipants, minnbr, date, time, message, headline)){
+		if(databaseManager.addNewActivity(owner, location, subcategory, maxnbr, minnbr, date, time, message, headline)){
 			//Det gick att skapa den nya aktiviteten
 			String sendMessage = "Activity: " + "created succesfully";
 			String confirmation_type = "OK";
